7e5f938360396598da66da3f71da3bcb3cc08e3a
baldo-vela <baldo.vela+github@gmail.com>
2020-11-17 03:14:32 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    #person name methods
+    def name=(persons_name)
+        @name = persons_name
+    end
+
+    def name
+        @name
+    end
+
+    #job methods
+    def job=(persons_job)
+        @job = persons_job
+    end
+
+    def job
+        @job
+    end 
+end
